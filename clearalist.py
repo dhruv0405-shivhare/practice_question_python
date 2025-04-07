4609bc4c902aca02121181e99a1348921faf68d0
Dhruv <dshivhare661@gmail.com>
2025-04-07 17:57:19 +0530

    questions

---
+++ clearalist.py
@@ -0,0 +1,23 @@
+# using clear() method
+
+mylist=[5,6,9,8,6]
+
+mylist.clear()
+
+print(mylist)
+
+# by initializes the list with no value
+
+mylist1=[5,6,9,3,4]
+
+print("before clear",mylist1)
+
+mylist1=[]
+print(mylist1)
+
+# using "*=0" this method removes all element of the list
+
+mylist2=[5,9,6,3,5]
+
+mylist2 *=0
+print(mylist2)
---
+++ reversealist.py
@@ -0,0 +1,11 @@
+# using built-in function
+mylist = [51,6,95,5,6,5,3,2]
+mylist.reverse
+print(mylist)
+
+# by using slicing technique
+
+mylist1=[2,6,9,8,7,5]
+
+mylist2=mylist1[::-1]
+print(mylist2)
